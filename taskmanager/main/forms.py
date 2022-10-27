a558efc177de8f41a08c3964b1b8c8cd2cf601fe
gr413_shivol <vlad.shilov03@mail.ru>
2022-10-27 11:59:47 +0700

    Регистрация готова

--- taskmanager/main/forms.py
+++ taskmanager/main/forms.py
@@ -2,26 +2,20 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from .models import AdvUser
+
+
 class UserRegisterForm(UserCreationForm):
     last_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-control'}))
     middle_name = forms.CharField(label='Отчество', widget=forms.TextInput(attrs={'class': 'form-control'}))
     username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(label='Email',widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Подтверждение пароля',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     checkbox = forms.BooleanField(label='Согласие на обработку персональных данных')
 
-
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='This field is required')
-
-
-
     class Meta:
-        model = User
+        model = AdvUser
         fields = ('last_name', 'first_name', 'middle_name', 'username', 'email', 'password1', 'password2', 'checkbox')
-
-
-        fields = ('username', 'email', 'password1', 'password2',)
-
