32e9c7593a05e162b31700caee5d83f6668391bb
Vladislav <vlad.shilov03@mail.ru>
2022-10-29 00:02:09 +0700

    Обьявления готовы

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -162,24 +162,24 @@ def profile_bb_add(request):
 
 @login_required
 def profile_bb_change(request, pk):
-   bb = get_object_or_404(Bb, pk=pk)
-   if not request.user.is_author(bb):
-       return redirect('main/profile')
-   if request.method == 'POST':
-       form = BbForm(request.POST, request.FILES, instance=bb)
-       if form.is_valid():
-           bb = form.save()
-           formset = AIFormSet(request.POST, request.FILES, instance=bb)
-           if formset.is_valid():
-               formset.save()
-               messages.add_message(request, messages.SUCCESS,
-                                    'Объявление изменено')
-               return redirect('main/profile')
-   else:
-       form = BbForm(instance=bb)
-       formset = AIFormSet(instance=bb)
-   context = {'form': form, 'formset': formset}
-   return render(request, 'rubric/profile_bb_change.html', context)
+    bb = get_object_or_404(Bb, pk=pk)
+    if not request.user.is_author(bb):
+        return redirect('main:profile')
+    if request.method == 'POST':
+        form = BbForm(request.POST, request.FILES, instance=bb)
+        if form.is_valid():
+            bb = form.save()
+            formset = AIFormSet(request.POST, request.FILES, instance=bb)
+            if formset.is_valid():
+                formset.save()
+                messages.add_message(request, messages.SUCCESS,
+                                     'Объявление изменено')
+                return redirect('main:profile')
+    else:
+        form = BbForm(instance=bb)
+        formset = AIFormSet(instance=bb)
+    context = {'form': form, 'formset': formset}
+    return render(request, 'rubric/profile_bb_change.html', context)
 
 @login_required
 def profile_bb_delete(request, pk):
@@ -190,7 +190,7 @@ def profile_bb_delete(request, pk):
        bb.delete()
        messages.add_message(request, messages.SUCCESS,
                             'Объявление удалено')
-       return redirect('main/profile')
+       return redirect('profile')
    else:
        context = {'bb': bb}
        return render(request, 'rubric/profile_bb_delete.html', context)
